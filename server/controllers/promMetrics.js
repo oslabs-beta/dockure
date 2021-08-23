3a378dc7202f2a8f37eae5c605f0ec49c53f3d30
n-tracy1 <n.tracy@outlook.com>
2021-08-23 13:45:01 -0400

    starting prometheus container is finished plus added assets and yml file boilerplate(unfinished)

--- server/controllers/promMetrics.js
+++ server/controllers/promMetrics.js
@@ -1,3 +1,5 @@
+// const util = require('util');
+// const exec = util.promisify(require('child_process').exec)  
 const { exec } = require('child_process');
 
 const promMetricsController = {};
@@ -9,10 +11,31 @@ const promMetricsController = {};
     // docker run -p 9090:9090 -v .yml:etc/prometheus/prometheus.yml prom/prometheus
 //
 promMetricsController.startProm = (req, res, next) => {  
-    //else run exec to start it on port 9090
     console.log('Entered PromMetricsController.startProm');
-    let prometheusStart = exec('docker run -p 9090:9090 prom/prometheus', (error, stdout, stderr) => {
+    console.log('Res.locals.running came through: ', res.locals.running );
+    if (res.locals.running) return next();
+    exec('docker run -p 9090:9090 prom/prometheus', (error, stdout, stderr) => {
         console.log('Entered prometheusStart');
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return next(error);
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return next(stderr);
+        };
+        // console.log(stdout);
+    });    
+    return next();
+}
+
+promMetricsController.checkProm = async (req, res, next) => {
+    console.log('entered CheckProm');
+    let running = false;
+    
+    exec('docker ps', (error, stdout, stderr) => {
+        console.log('Check if Prom is running');
+        
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -21,25 +44,64 @@ promMetricsController.startProm = (req, res, next) => {
             console.log(`stderr: ${stderr}`);
             return;
         };
-        console.log(stdout);
-    });
-    setTimeout(() => {
-        exec('docker ps', (error, stdout, stderr) => {
-                console.log('Entered second part');
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
+        // console.log(stdout);
+        
+        const target = 'prom/prometheus';
+        for (let i = 0; i < stdout.length && running === false; i++) {
+            if (stdout[i] === 'p') {
+                for (let j = 0; j < target.length; j++) {
+                    const targetLetter = target[j];
+                    const containersLetter = stdout[i];
+                    if (targetLetter === containersLetter && target.length - 1 === j) {
+                        running = true;
+                    }
+                    else if (targetLetter !== containersLetter) {
+                        break;
+                    }
+                    i++;
                 }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                };
-                console.log(stdout);
-                res.send('itworked!');
-            })
-    }, 2000)
-    
-    return next();
+            }
+        }
+        res.locals.running = running;
+        return next();
+    })
 }
 
+
+
+
+
 module.exports = promMetricsController;
+
+// exec("docker image ls", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+
+//     const result = stdout.split('\n');
+//     let values = [];
+//     result.forEach(ele => {
+//     values.push(ele.split(" ").filter(item => item !== ""));    
+// })
+//     const keys = values.shift();
+
+//     values = values.map(function (element) {
+//         const obj = {}; 
+//         keys.forEach(function (key, index) {
+//             if(element.length){
+//             obj[key] = element[index];
+//         }
+//     });
+//     return obj;
+// });
+//     if(JSON.stringify(values[values.length - 1]) === '{}') values.pop(); 
+//     console.log(values)
+// });
+
+
+
--- server/routers/promMetrics.js
+++ server/routers/promMetrics.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const promMetricsController = require('../controllers/promMetrics');
 const promMetricsControllers = require('../controllers/promMetrics');
 const promMetricsRouter = express.Router();
 
-promMetricsRouter.get('/promStart', promMetricsControllers.startProm, (res, req) => {
+promMetricsRouter.get('/promStart', 
+    promMetricsControllers.checkProm,
+    promMetricsControllers.startProm,
+    (req, res) => {
     console.log('Finished PromMetricsController');
-    // return res.status(200).send('It worked!');
-})
+    return res.status(200).send('it worked!');
+});
 
 module.exports = promMetricsRouter;
