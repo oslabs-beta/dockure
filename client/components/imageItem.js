3774d90615a9f5ea25366d34cb419722ce1840f2
LPT <lptalty@gmail.com>
2021-08-24 14:24:50 -0400

    Added buttons to each image before check in, next will have to create functionality and possiblity styling as they are currently only text

--- client/components/imageItem.js
+++ client/components/imageItem.js
@@ -5,6 +5,10 @@ console.log(image);
     return (
     <li className="image_item">
         <div className='image-name'>{image.RepoTags}</div>
+        <span className="image_button">
+        <button>Start</button>
+        <button>Stop</button>
+        </span>
     </li>
     )
 
