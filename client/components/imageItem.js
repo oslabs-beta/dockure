2004050ad5df9eccd2d37d6b51308f5a6e1d0ee8
LPT <lptalty@gmail.com>
2021-08-24 16:01:53 -0400

    Every unnamed image that is imported gets updated to Anonymous on the page

--- client/components/imageItem.js
+++ client/components/imageItem.js
@@ -3,19 +3,23 @@ import React, { component } from 'react';
 const ImageItem = ({id, image}) => {
 
 
-console.log(image);
+// console.log(image);
 
 const checkRepoTag = ({image}) => {
-    if(image.RepoTags === '<none>:<none>') image.RepoTags = 'Anonymous';
+
+    //at index zero because it is an array which is the only way to compare with string value
+    if(image.RepoTags[0] === '<none>:<none>'){
+        image.RepoTags = ['Anonymous'];
+    } 
     return image.RepoTags;
 }
     return (
     <li className="image_item">
-        <div className='image-name'>{image.RepoTags}</div>
-        <span className="image_button">
+        <div className='image-name'>{checkRepoTag({image})}</div>
+        <div className="image_button">
         <button>Start</button>
         <button>Stop</button>
-        </span>
+        </div>
     </li>
     )
 
