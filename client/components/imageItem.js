1387c1b094dd37b1e58e29f1335664c3397f4f0d
LPT <lptalty@gmail.com>
2021-08-24 15:02:37 -0400

    committing before I pull dev

--- client/components/imageItem.js
+++ client/components/imageItem.js
@@ -1,7 +1,14 @@
 import React, { component } from 'react';
 
 const ImageItem = ({id, image}) => {
+
+
 console.log(image);
+
+const checkRepoTag = ({image}) => {
+    if(image.RepoTags === '<none>:<none>') image.RepoTags = 'Anonymous';
+    return image.RepoTags;
+}
     return (
     <li className="image_item">
         <div className='image-name'>{image.RepoTags}</div>
