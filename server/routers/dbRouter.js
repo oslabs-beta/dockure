a6da19371fee3a6c2c342aa3dc489ff771abdf24
Alex Zayas <zayas.alexander@gmail.com>
2021-08-25 18:27:32 -0400

    mainfunctionality of controllers and routers done

--- server/routers/dbRouter.js
+++ server/routers/dbRouter.js
@@ -18,40 +18,12 @@ dbController.userLogin,
     return res.status(200).send({userid: res.locals.userid});
 });
 
-router.post('/logout',
-dbController.logout,
-(req, res) => {
-    return res.status(200).redirect('/')
-});
+// router.post('/logout',
+// dbController.logout,
+// (req, res) => {
+//     return res.status(200).redirect('/')
+// });
 
 module.exports = router;
 
 
-// userRouter.post(
-//     "/signup",
-//     userController.createUser,
-//     cookieController.setCookies,
-//     (_req, res) => {
-//       res.status(200).send({ id: res.locals.id });
-//     }
-//   );
-  
-//   userRouter.post(
-//     "/login",
-//     userController.loginUser,
-//     cookieController.setCookies,
-//     (_req, res) => {
-//       res.status(200).send({ id: res.locals.id });
-//     }
-//   );
-// 
-//   // Eliminates cookie and sends user back to home page.
-//   userRouter.post(
-//     "/logout",
-//     cookieController.unsetCookies,
-//     questionController.setInactive,
-//     (_req, res) => {
-//       res.status(200).redirect("/");
-//     }
-//   );
-  
