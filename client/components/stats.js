984bde8f264f30274eac322ce444d2399292ab1f
n-tracy1 <n.tracy@outlook.com>
2021-08-25 16:16:56 -0400

    pre merge in after hiring lecture

--- client/components/stats.js
+++ client/components/stats.js
@@ -1,16 +1,27 @@
-import React, { component } from 'react';
+import React, { component, useEffect, useState } from 'react';
 import StatsService from '../services/statsService';
+import recharts from 'recharts';
 
 const Stats = () => {
+  //add some state for the data
+  const [memory, setMemory] = useState([])
+  //use effect on load to update data
+
+  //button to update data
+  useEffect(async () => {
+    const result = await StatsService.getStats('process_resident_memory_bytes', 1);
+    setMemory(result.data);
+  }, []);
 
   function test() {
-    return StatsService.getStats('node_memory_Active_bytes', 1);
+    console.log(memory);
+    // return StatsService.getStats('process_resident_memory_bytes', 1);
   }
 
   return (
     <div className='stats'>
       {/* <div className='stats_cpu'>CPU</div> */}
-      <button onClick={test} >StatsTest</button>
+      <button onClick={test} >Refresh</button>
       <div className='stats_memory'>Memory</div>
       {/* <div className='stats_count'>Request Count</div> */}
     </div>
