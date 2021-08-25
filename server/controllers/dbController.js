71473f4d375610c2bf664dd0a335691ea95a3764
Alex Zayas <zayas.alexander@gmail.com>
2021-08-25 17:23:53 -0400

    finished routers and a portion of the controllers

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -2,68 +2,51 @@ const db = require('../models/dockureModel');
 
 const dbController = {};
 
-dbController.createUser = (req, res, next) => {
-    db.query(`SELECT userID FROM user WHERE userName="${req.body.username}"`)
-    //if user already exists return error username already exists
-    //else
-      .then((response) => {
+dbController.createUser = async (req, res, next) => {
+  const { username, password } = req.body;
+    if(!username || !password)
+     return next({
+      status: 401,
+      errMessage: "invalid username or password", 
+    });
+     
+    let hashedPassword;
+    try {
+          const salt = bcrypt.genSaltSync(10);
+          hashedPassword = bcrypt.hashSync(password, salt);
+        } catch (err) {
+          return next({ status: 500, message: err.message, message2: 'password error' });
+        }
+
         
+        
+        try {
+          const params = [username, hashedPassword];
+          const query = `INSERT INTO user (userid ,username, password) VALUES ($1, $2) RETURNING userid` ;
+          const { rows } = await pool.query(query, params);
+          res.locals.userid = rows[0].userid;
+        } catch (err) {
+        return next({
+          status: 500,
+          message:"Username already exists",
+        });
+      }
+      return next();
+    }; //
+      
+      
+      dbController.userLogin = async (req, res, next) => {\
+      const { username, password } = req.body;
+      if (!username || !password)
+          return next(new Error("Please enter username and password"));
 
-        return next();
-      })
-      .catch((err) => {
-        if (err) return next(err);
-      });
-  };
+          let user;
 
-  dbController.createUser = (req, res, next) => {
-      //query db for username if does not exist we can add to db and assign unique user_id
-      db.query(user_id)
-
-  }
-
-
-module.exports = dbController;
-
-
-// userController.createUser = async (req, res, next) => {
-//   // Get username and password from body of POST request
-//   const { username, password } = req.body;
-//   if (!username || !password)
-//     return next({
-//       status: 401,
-//       message: "Invalid username or password..",
-//     });
-
-//   // Create hashed version of password.
-//   let hashedPassword;
-//   try {
-//     const salt = bcrypt.genSaltSync(10);
-//     hashedPassword = bcrypt.hashSync(password, salt);
-//   } catch (err) {
-//     return next({ status: 500, message: err.message, message2: 'not working' });
-//   }
-
-//   // Save user to PostgreSQL database, using parameterized queries.
-//   try {
-//     const params = [username, hashedPassword];
-//     const query = `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id`;
-//     const { rows } = await pool.query(query, params);
-//     res.locals.id = rows[0].id; // Assign "ID" of created user to res.locals.id, so that our anonymous function in the router can pass it back to the user.
-//   } catch (err) {
-//     return next({
-//       status: 500,
-//       message:
-//         "Could not sign up user with that username and password. Do you already have an account?",
-//     });
-//   }
-
-//   // Move to the next middleware after insertion is complete.
-//   return next();
-// };
+      }; //
 
 
 
+      module.exports = dbController;
 // userController.loginUser = async (req, res, next) => {
 //   const { username, password } = req.body;
 //   if (!username || !password)
@@ -103,3 +86,42 @@ module.exports = dbController;
 //   return next();
 // };
   
+      
+// userController.createUser = async (req, res, next) => {
+  //   // Get username and password from body of POST request
+//   const { username, password } = req.body;
+//   if (!username || !password)
+//     return next({
+  //       status: 401,
+  //       message: "Invalid username or password..",
+//     });
+
+//   // Create hashed version of password.
+//   let hashedPassword;
+//   try {
+//     const salt = bcrypt.genSaltSync(10);
+//     hashedPassword = bcrypt.hashSync(password, salt);
+//   } catch (err) {
+//     return next({ status: 500, message: err.message, message2: 'not working' });
+//   }
+
+//   // Save user to PostgreSQL database, using parameterized queries.
+//   try {
+//     const params = [username, hashedPassword];
+//     const query = `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id`;
+//     const { rows } = await pool.query(query, params);
+//     res.locals.id = rows[0].id; // Assign "ID" of created user to res.locals.id, so that our anonymous function in the router can pass it back to the user.
+//   } catch (err) {
+//     return next({
+//       status: 500,
+//       message:
+//         "Could not sign up user with that username and password. Do you already have an account?",
+//     });
+//   }
+
+//   // Move to the next middleware after insertion is complete.
+//   return next();
+// };
+
+
+
--- server/routers/dbRouter.js
+++ server/routers/dbRouter.js
@@ -6,10 +6,52 @@ const router = express.Router();
 
 //route handler
 
-router.post('/user',
-dbController.,
+router.post('/signup',
+dbController.createUser,
 (req, res) => {
-    return res.status(200).json('Database initialized')
+    return res.status(200).send({userid: res.locals.userid});
+});
+
+router.post('/login',
+dbController.userLogin,
+(req,res) => {
+    return res.status(200).send({userid: res.locals.userid});
+});
+
+router.post('/logout',
+dbController.logout,
+(req, res) => {
+    return res.status(200).redirect('/')
 });
 
 module.exports = router;
+
+
+// userRouter.post(
+//     "/signup",
+//     userController.createUser,
+//     cookieController.setCookies,
+//     (_req, res) => {
+//       res.status(200).send({ id: res.locals.id });
+//     }
+//   );
+  
+//   userRouter.post(
+//     "/login",
+//     userController.loginUser,
+//     cookieController.setCookies,
+//     (_req, res) => {
+//       res.status(200).send({ id: res.locals.id });
+//     }
+//   );
+// 
+//   // Eliminates cookie and sends user back to home page.
+//   userRouter.post(
+//     "/logout",
+//     cookieController.unsetCookies,
+//     questionController.setInactive,
+//     (_req, res) => {
+//       res.status(200).redirect("/");
+//     }
+//   );
+  
