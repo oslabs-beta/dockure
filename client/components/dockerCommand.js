068f0a72b3a975b9cc621e7a2c51cc99831e6961
LPT <lptalty@gmail.com>
2021-08-26 15:38:16 -0400

    Styling for the front end, mostly within the container item and container list files

--- client/components/dockerCommand.js
+++ client/components/dockerCommand.js
@@ -5,20 +5,20 @@ const DockerCommand = ({conList}) => {
   return (
     <div className='docker_command'>
       <ul className='docker_buttons'>
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Kill</button>
-        <button>Restart</button>
-        <button>Pause</button>
-        <button>Resume</button>
-        <button>Remove</button>
-        <Link to='/main/create'>
-          <button>
-            <span>+</span>
-            <span>Add container</span>
+        <button className='docker_btn'>Start</button>
+        <button className='docker_btn'>Stop</button>
+        <button className='docker_btn'>Kill</button>
+        <button className='docker_btn'>Restart</button>
+        <button className='docker_btn'>Pause</button>
+        <button className='docker_btn'>Resume</button>
+        <button className='docker_btn'>Remove</button>
+      </ul>
+      <Link to='/main/create'>
+      <button className='docker_btn add_btn'>
+            {/* <span>+</span> */}
+            <span>Add Container</span>
           </button>
         </Link>
-      </ul>
       {/* get data of the which container got picked, and apply the buttons // or
       we can send all the button functions to the containerlist as props. */}
       <ContainerList conList={conList} />
