76a41caefc7061866a82bb55c3b355c89e80c311
Hazel <hazel.na3@gmail.com>
2021-09-07 11:05:53 -0400

    color theme 1

--- client/components/imageItem.js
+++ client/components/imageItem.js
@@ -1,11 +1,12 @@
 import React, { component, useState, useEffect } from 'react';
 import imageService from '../services/imageService';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const ImageItem = ({ id, image }) => {
-    const [isRunning, setIsRunning] = useState();
-    const [clickStart, setClickStart] = useState(0);
-  
+  const [isRunning, setIsRunning] = useState();
+  const [clickStart, setClickStart] = useState(0);
+
   const checkRepoTag = ({ image }) => {
     //at index zero because it is an array which is the only way to compare with string value
     if (image.RepoTags[0] === '<none>:<none>') {
@@ -17,11 +18,13 @@ const ImageItem = ({ id, image }) => {
   };
 
   const startClick = async (e) => {
-        const ID = { image }.image.Id.slice(7, 19);
-        const handleSubmit = await imageService.startImage('http://localhost:3000/api/images/start', ID);
-        if (handleSubmit.data === 'running') setIsRunning(true);
+    const ID = { image }.image.Id.slice(7, 19);
+    const handleSubmit = await imageService.startImage(
+      'http://localhost:3000/api/images/start',
+      ID
+    );
+    if (handleSubmit.data === 'running') setIsRunning(true);
   };
-
 
   return (
     <div className='image_item'>
@@ -32,9 +35,11 @@ const ImageItem = ({ id, image }) => {
           <div className='image_running'>In Use</div>
         ) : (
           <div>
-            <button className='image_button image_start' onClick={startClick}>
-              Start
-            </button>
+            <Link to='/main'>
+              <button className='image_button image_start' onClick={startClick}>
+                Start
+              </button>
+            </Link>
           </div>
         )}
         {/* <div>
